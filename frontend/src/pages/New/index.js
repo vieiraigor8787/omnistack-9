2bb222d01ece9dcdf3dca279bcc739d6ceddf0a8
Igor Cerqueira <igor_vc@hotmail.com>
2019-10-03 15:38:51 -0300

    new: inicio/css/form/

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,6 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function New() {
-    return <div/>
+    const [company, setCompany] = useState('')
+    const [techs, setTechs] = useState('')
+    const [price, setPrice] = useState('')
+
+    function handleSubmit() {
+
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="company">EMPRESA *</label>
+            <input 
+            type="text"
+            placeholder="Sua empresa incrível"
+            value={company}
+            onChange={event => setCompany(event.target.value)}
+            />
+
+            <label htmlFor="techs">TECNOLOGIAS *<span>(separadas por vírgula)</span> 
+            </label>
+            <input 
+            type="text"
+            placeholder="Tecnologias utilizadas"
+            value={techs}
+            onChange={event => setTechs(event.target.value)}
+            />
+
+            <label htmlFor="price">VALOR DIÁRIA *<span>(em branco se for gratuito)</span> 
+            </label>
+            <input 
+            type="text"
+            placeholder="Tecnologias utilizadas"
+            value={price}
+            onChange={event => setPrice(event.target.value)}
+            />
+
+            <button type="submit" className="btn">Cadastrar</button>
+        </form>
+    )
 }
 
